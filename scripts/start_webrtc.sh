5deaaea573d392fc56825dc3df42e102f1b5802b
catid <mrcatid@gmail.com>
2020-12-03 06:32:35 +0000

    fix

--- scripts/start_webrtc.sh
+++ scripts/start_webrtc.sh
@@ -13,6 +13,6 @@ SCRIPT=`realpath $0`
 SCRIPT_PATH=`dirname ${SCRIPT}`
 PLUGIN_PATH=`realpath ${SCRIPT_PATH}/../build/kvm_janus/libkvm_janus.so`
 
-rm plugins/*.so
+rm -f plugins/*.so
 ln -s ${PLUGIN_PATH} plugins
 janus -F . -P ./plugins
